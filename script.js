5519ae42e4ade5d934e12b139cf8ddf95406f617
itsoliviasparks <itsoliviasparks@gmail.com>
2023-01-13 14:45:51 -0500

    updated API calls, reorganized JS file

--- script.js
+++ script.js
@@ -17,29 +17,52 @@
             //add quote to page
                 //add author to page
 
+    // On content page load - the timer starts
+    // timer starts at 1:00 minute 
+    // Every second - the timer count goes down 
+    //The current time gets displayed on screen 
+    //After the display - the timer count goes down again 
+    // When count hits 0 - the timer stops 
+    // The reset button can refresh the timer back to its original state - 1:00 minute 
+
 const meditationMinute = {}; 
+
+meditationMinute.getRandomItemInArr = (arr) => {
+    const randomNumber = Math.floor(Math.random() * arr.length)
+    return arr[randomNumber];
+};
+
+meditationMinute.timerDisplay = (currentTime) => {
+     const h2Element = document.querySelector("h2");
+     h2Element.innerHTML = `00:${currentTime}`;
+}
+
+meditationMinute.timer = () => {
+    let currentTime = 60;
+    setInterval( function(){
+        if (currentTime <= 60){
+            currentTime--;
+            if (currentTime > 0) {
+                meditationMinute.timerDisplay(currentTime);
+            } else {
+                window.location.reload(); 
+            }
+        }
+    },1000)
+}
+
+meditationMinute.restart = () => {
+    const restartButton = document.querySelector(".restart");
+    restartButton.addEventListener("click", () => {
+        window.location.reload(); 
+    });
+};
 
 meditationMinute.updateQuoteContent = () => {
     const quoteElement = document.querySelector(".quote");
     const authorElement = document.querySelector(".author");
     quoteElement.innerHTML = `"${meditationMinute.quoteContent}"`;
     authorElement.innerHTML = `- ${meditationMinute.quoteAuthor}`;
-};
-
-//API call to Zen Quotes
-meditationMinute.getQuote = () => {
-    const url = `https://proxy-ugwolsldnq-uc.a.run.app/https://zenquotes.io/api/random/`
-    fetch(url)
-    .then((response) => {
-        return response.json();
-    }).then((jsonData) => {
-        //save random quote in variable
-        meditationMinute.quoteContent = jsonData[0].q;
-        //save quote author in variable   
-        meditationMinute.quoteAuthor = jsonData[0].a;
-        //update the quote content
-        // meditationMinute.updateQuoteContent();
-    })
 };
 
 meditationMinute.updateArtContent = () => {
@@ -53,70 +76,8 @@ meditationMinute.updateArtContent = () => {
     `;
 };
 
-
-//API call to Rjiks Art Images
-meditationMinute.getArt = () => {
-    const url = new URL('https://www.rijksmuseum.nl/api/en/collection');
-    const apiKey = 'aw5uplA6';
-    url.search = new URLSearchParams({
-    key: apiKey,
-    p: 1000,
-    imgonly: true,
-    q: "painting",
-    })
-    fetch(url)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(jsonData) {
-        const artArr = jsonData.artObjects;
-        const randomArt = meditationMinute.getRandomItemInArr(artArr);
-        //save random art image in variable
-        meditationMinute.artImage = randomArt.webImage.url;
-        //save that image's title & artist in separate variable
-        // Title
-       meditationMinute.artTitle = randomArt.title;
-        // Artist
-        meditationMinute.artMaker = randomArt.principalOrFirstMaker;
-        // meditationMinute.updateArtContent();
-    })
-};
-
-meditationMinute.restart = () => {
-    const restartButton = document.querySelector('.restart');
-    restartButton.addEventListener("click", () => {
-        window.location.reload(); 
-    });
-
-};
-
-meditationMinute.getRandomItemInArr = (arr) => {
-    const randomNumber = Math.floor(Math.random() * arr.length)
-    return arr[randomNumber];
-};
-
-
-// Timer 
-// meditationMinute.timer = () => {
-
-// }
-// Have event listener on the home page button 
-meditationMinute.homeButtonListener = () => {
-    const homeButton = document.querySelector('.home');
-    // When the button gets clicked 
-    homeButton.addEventListener('click', () => {
-            // update the content of the page 
-            meditationMinute.updateDisplayedContent();
-            meditationMinute.updateArtContent();
-            meditationMinute.updateQuoteContent();
-            meditationMinute.restart();
-
-            // The update will trigger the timer 
-    })
-}
-
 meditationMinute.updateDisplayedContent = () => {
-    const mainElement = document.querySelector('main');
+    const mainElement = document.querySelector("main");
     mainElement.innerHTML = `
     <header>
 			<h2>1:00</h2>
@@ -137,47 +98,55 @@ meditationMinute.updateDisplayedContent = () => {
         meditationMinute.timer();
 }
 
+//API call to Rjiks Art Images
+meditationMinute.getArt = () => {
+    const url = new URL("https://www.rijksmuseum.nl/api/en/collection");
+    const apiKey = "aw5uplA6";
+    url.search = new URLSearchParams({
+    key: apiKey,
+    p: 1000,
+    imgonly: true,
+    q: "painting",
+    })
+    
+    async function getFromAPI() {
+        const respo = await fetch(url);
+        const artArr = await respo.json();
+        const randomArt = meditationMinute.getRandomItemInArr(artArr.artObjects);
+        meditationMinute.artImage = randomArt.webImage.url;
+        meditationMinute.artTitle = randomArt.title;
+        meditationMinute.artMaker = randomArt.principalOrFirstMaker;
+    }
+    getFromAPI();
+};
 
-// On content page load - the timer starts
-// timer starts at 1:00 minute 
-// Every second - the timer count goes down 
-//The current time gets displayed on screen 
-//After the display - the timer count goes down again 
-// When count hits 0 - the timer stops 
-// The reset button can refresh the timer back to its original state - 1:00 minute 
+//API call to Zen Quotes
+meditationMinute.getQuote = () => {
+    const url = "https://proxy-ugwolsldnq-uc.a.run.app/https://zenquotes.io/api/random/"
 
-
-meditationMinute.timer = () => {
-    let currentTime = 3;
-    setInterval( function(){
-        if (currentTime <= 3){
-            currentTime--;
-            if (currentTime > 0) {
-                // console.log(currentTime);
-                meditationMinute.timerDisplay(currentTime);
-            } else {
-                window.location.reload(); 
-            }
-        }
-    },1000)
+    async function getFromAPI() {
+        const respo = await fetch(url);
+        const quote = await respo.json();
+        meditationMinute.quoteContent = quote[0].q;
+        meditationMinute.quoteAuthor = quote[0].a;
+    }
+    getFromAPI();
 }
 
-meditationMinute.timerDisplay = (currentTime) => {
-     const h2Element = document.querySelector('h2');
-     h2Element.innerHTML = `00:${currentTime}`;
-    // console.log('it works')
+meditationMinute.homeButtonListener = () => {
+    const homeButton = document.querySelector(".home");
+    homeButton.addEventListener("click", () => {
+        meditationMinute.updateDisplayedContent();
+        meditationMinute.updateArtContent();
+        meditationMinute.updateQuoteContent();
+        meditationMinute.restart();
+    })
 }
-
-
 
 meditationMinute.init = () => {
-    meditationMinute.homeButtonListener();
     meditationMinute.getQuote();
-    meditationMinute.getArt();    
-    //we need a timer function
-    // stretch goal of colors
-    //throwing and catching errors
+    meditationMinute.getArt();
+    meditationMinute.homeButtonListener();
 };
 
 meditationMinute.init();
-
